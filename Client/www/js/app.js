e3c1f58c37f2023ff2484e3973766544094466af
Tobias Brosge <s0539713@htw-berlin.de>
2015-09-25 12:12:48 +0200

    begin with storage

--- Client/www/js/app.js
+++ Client/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.constants'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -49,7 +49,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
-  
+
   .state('app.locations', {
     url: '/locations',
     views: {
---
+++ Client/www/js/constants.js
@@ -0,0 +1,5 @@
+angular.module('starter.constants', [])
+
+.constant('serverSettings', {
+  url: 'http://localhost/api/v1/api.php/'
+});
