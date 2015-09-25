87245bf0dfb44dc52f5f30fea1e42d7885dbdb47
Tobias Brosge <s0539713@htw-berlin.de>
2015-09-25 12:13:18 +0200

    begin with storage

--- Client/www/js/controllers.js
+++ Client/www/js/controllers.js
@@ -7,32 +7,49 @@ angular.module('starter.controllers', [])
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
-  //});  
+  //});
 })
 
 .controller('LocationsCtrl', function($scope, sharedProperties) {
-  $scope.locations = sharedProperties.getProperty();
+  $scope.locations = sharedProperties.get();
   //console.log($scope.locations);
 })
 
-.controller('LocationCtrl', function($scope, sharedProperties) {
-  $scope.locations = sharedProperties.getProperty();
+.controller('LocationCtrl', function($scope, pois, sharedProperties) {
+  $scope.locations = sharedProperties.get();
 })
 
-.service('sharedProperties', function() {
-  var locations = [
-    { title: 'Rabbitwhole', id: 1, pic: '/img/desert.png'},
-    { title: 'Spitzkoppe', id: 2, pic: '/img/desert.png'},
-    { title: 'Brandberg', id: 3, pic: '/img/desert.png'},
-    { title: 'Download new content', id: 4, pic: '/img/desert.png'}
-  ];
+.service('sharedProperties', function(serverSettings) {
+  var availableLocations = window.localStorage['availableLocations'];
+  var downloadedLocations = window.localStorage['downloadedLocations'];
+  //   { id: 1, pic: 'img/desert.png', title: 'Rabbitwhole'},
+  //   { id: 2, pic: 'img/desert.png', title: 'Spitzkoppe'},
+  //   { id: 3, pic: 'img/desert.png', title: 'Brandberg'},
+  //   { id: 4, pic: 'img/desert.png', title: 'Download new content'}
+  // ];
+  var getDownloadedLocations = function() { return downloadedLocations };
+  var getAvailableLocations = function() { return availableLocations; };
+  var updateLocations = function() {
+      return $http.updateLocations(serverSettings.url + 'Location').then(function(data) {
+          return availableLocations
+            = window.localStorage['availableLocations']
+            = data;
+    });
+  };
 
   return {
-    getProperty: function() {
-      return locations;
-    },
-    setProperty: function(locs) {
-      locations = locs;
+    getDownloadedLocations: getDownloadedLocations,
+    getAvailableLocations : getAvailableLocations,
+    updateLocations: updateLocations
+  };
+})
+
+.service('pois', function() {
+  var pois = [];
+
+  return {
+    get: function() {
+      return pois;
     }
   };
 });
